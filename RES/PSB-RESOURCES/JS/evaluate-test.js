24a2443043b97ee062d1ef978ba3c5834bc6e188
louiejavier0923 <43311871+louiejavier0923@users.noreply.github.com>
2019-01-28 14:29:25 +0800

    lou - psb
    psb-resources, including auto compute of points

---
+++ RES/PSB-RESOURCES/JS/evaluate-test.js
@@ -0,0 +1,202 @@
+getUSer($('.eval-user-info').attr('name'));
+getPublishedVacancy();
+
+$(document).ready(function(){
+
+	$('body')
+	.on('click','#exit',function(){
+		// logout button click
+		var confirmation = confirm('Are you sure you want to log out?');
+		if (confirmation) {
+			$.ajax({
+				type:'POST',
+				url:'../RES/PSB-RESOURCES/PHP/session-logout.php',
+				data:''
+			}).done(function(b){
+				if(b){
+					window.location.href = "../evaluator-login-form.php";
+				}
+			})
+		}
+	})
+	.on('keyup change blur','.applicant-filters',function(){
+
+		var job = $('#published_vacancy_select').val(),
+		applicant_name = $('#applicant_namesearch').val();
+
+		$.ajax({
+			type:'POST',
+			url:'../RES/PSB-RESOURCES/PHP/model.php',
+			data:{
+				action:'get_applicant_list-test',
+				a:job,
+				b:applicant_name,
+				c:user_id
+			}
+		}).done(function(data){
+			// console.log(data);
+			$('.content-info').html(data);
+			setEditableInputGrades($('.evaluator-criteria').html());
+		});
+
+	})
+	.on('blur','.input-grade',function(){
+
+		var lim,
+		grade = $(this).val(),
+		criteria = $(this).prop('name'),
+		pid = $('#published_vacancy_select').val();
+
+		switch(criteria){
+			case 'EDUCATION':
+				lim = 20;
+			break;
+
+			case 'EXPERIENCE':
+				lim = 15;
+			break;
+
+			case 'TRAINING':
+				lim = 10;
+			break;
+
+			case 'ELIGIBILITY':
+				lim = 15;
+			break;
+
+			case 'INTERVIEW':
+				lim = 10;
+			break;
+
+			case 'DEMO':
+				lim = 15;
+			break;
+
+			case 'COMMUNICATION':
+				lim = 15;
+			break;
+		}
+
+		if (grade<0 || grade>lim || grade=="") {
+			alert('Invalid grade entered!');
+			// $(this).focus();
+		}
+		else{
+
+			var uid = $(this).parent('.content').siblings('.content:first-child').children('.applicant-id').html();
+
+			// console.log(uid+' / '+criteria+' / '+grade+' / '+user_id);
+
+			$.ajax({
+				type:'POST',
+				url:'../RES/PSB-RESOURCES/PHP/model.php',
+				data:{
+					action:'insert_applicant_point',
+					a:uid,
+					b:criteria,
+					c:grade,
+					d:user_id,
+					e:pid,
+					val:''
+				}			
+			}).done(function(data){
+				// console.log(data);
+				if (data) {
+					$('.applicant-filters').blur();
+				}
+			})
+
+		}
+
+	})
+	.on('click','#btn_save',function(){
+
+		$('.applicant-filters').blur();
+
+	})
+	.on('click','.eval-out-editbtn',function(){
+
+		var e_win = $(this).parent('.eval-out').siblings('.educ-window');
+		// e_win.css({
+		// 	'display':'block'
+		// })
+		e_win.toggle();
+
+	})
+	.on('click','.ee-cancel',function(){
+
+		var e_win = $(this).parent('.ewc-grid2-col').parent('.ewc-grid2').parent('.educ-window-content').parent('.educ-window');
+		e_win.css({
+			'display':'none'
+		})
+	})
+	.on('click','.ee-save',function(){
+
+		var additional = 0;
+		
+		var e_win = $(this).parent('.ewc-grid2-col').parent('.ewc-grid2').parent('.educ-window-content').parent('.educ-window');
+		
+		var gwa = parseFloat($(this).parent('.ewc-grid2-col').siblings('.input-gwa').val()),
+		uid = $(this).siblings('.applicant-id').html(),
+		pid = $('#published_vacancy_select').val(),
+		master = $(this).parent('.ewc-grid2-col').parent('.ewc-grid2').siblings('.ewc-grid').children('.ewc-grid-input').children('input[type=checkbox].checkbox-master'),
+		doctor = $(this).parent('.ewc-grid2-col').parent('.ewc-grid2').siblings('.ewc-grid').children('.ewc-grid-input').children('input[type=checkbox].checkbox-doctor');
+		
+		if (master.is(':checked')==true && doctor.is(':checked')==false) {
+			additional = 1;
+		}
+		else if((master.is(':checked')==true && doctor.is(':checked')==true)||(master.is(':checked')==false && doctor.is(':checked')==true)){
+			additional = 2;
+		}
+
+
+		if (gwa>3 || gwa<1) {
+			alert('Invalid GWA! Enter a valid number!');
+		}
+		else{
+			var val = gwa.toString()+','+additional.toString();
+			console.log(val)
+			$.ajax({
+				type:'POST',
+				url:'../RES/PSB-RESOURCES/PHP/model.php',
+				data:{
+					action:'insert_applicant_point',
+					a:uid,
+					b:'EDUCATION',
+					c:parseFloat(educationPoints('7',gwa))+parseFloat(additional),
+					d:user_id,
+					e:pid,
+					val:val
+				}			
+			}).done(function(data){
+				// console.log(data);
+				if (data) {
+					$('.applicant-filters').blur();
+				}
+			}) 
+		}
+	})
+	.on('click','.checkbox-master,.checkbox-doctor',function(){
+		switch($(this).attr('name')){
+
+			case 'master':
+				if ($(this).is(':checked')==true) {
+					$(this).parent('.ewc-grid-input').siblings('ewc-grid-input:last-child').children('.checkbox-doctor').prop('checked',false);
+				}
+			break;
+
+			case 'doctor':
+				if ($(this).is(':checked')==true) {
+					$(this).parent('.ewc-grid-input').siblings('ewc-grid-input:first-child').children('.checkbox-master').prop('checked',false);
+				}
+			break;
+
+		}
+
+	});
+
+	setTimeout(function(){
+		$('.applicant-filters').blur();
+	},1000);
+
+});
--- RES/PSB-RESOURCES/PHP/model.php
+++ RES/PSB-RESOURCES/PHP/model.php
@@ -152,6 +152,7 @@ switch ($_POST['action']) {
 		$c = $_POST['c'];
 		$d = $_POST['d'];
 		$e = $_POST['e'];
+		$f = $_POST['val'];
 
 		$sql = "SELECT NO FROM applicants_points WHERE UID='$a' AND CRITERIA_CODE='$b' AND PID='$e' AND CRITERIA_CODE!='INTERVIEW';";
 		$result = mysqli_query($conn,$sql);
@@ -159,19 +160,156 @@ switch ($_POST['action']) {
 
 		if ($rowcount!=1) {
 			
-			$sql = "INSERT INTO applicants_points(UID,PID,CRITERIA_CODE,EQUIVALENT_POINTS,GRADED_BY) VALUES ('$a','$e','$b','$c','$d');";
+			$sql = "INSERT INTO applicants_points(UID,PID,CRITERIA_CODE,EQUIVALENT_POINTS,GRADED_BY,VALUE) VALUES ('$a','$e','$b','$c','$d','$f');";
 			$result = mysqli_query($conn,$sql);
 
 		}
 		else{
 
-			$sql = "UPDATE applicants_points SET EQUIVALENT_POINTS='$c' WHERE UID='$a' AND PID='$e' AND CRITERIA_CODE='$b' AND GRADED_BY='$d';";
+			$sql = "UPDATE applicants_points SET EQUIVALENT_POINTS='$c',VALUE='$f' WHERE UID='$a' AND PID='$e' AND CRITERIA_CODE='$b' AND GRADED_BY='$d';";
 			$result = mysqli_query($conn,$sql);
 
 		}
 		print_r($result);
 
 	break;
+
+
+	// testing ------------------------------------------------------------------------------------
+
+	case 'get_applicant_list-test':
+
+		$a = $_POST['a'];
+		$b = $_POST['b'];
+		$c = $_POST['c'];
+
+		$html = "";
+		$cnt = 0;
+
+		$sql = "SELECT p.UID,p.LASTNAME,p.FIRSTNAME,p.MIDDLENAME,p.EXTENSION_NAME,p.RESIDENTIAL_ADDRESS,p.MOBILE_NO,u.EMAIL,
+			(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='EDUCATION' AND ap.UID=a.UID) AS 'EDUCATION',
+			(SELECT ap.VALUE FROM applicants_points ap WHERE ap.CRITERIA_CODE='EDUCATION' AND ap.UID=a.UID) AS 'EDUCATION_GWA',
+			(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='EXPERIENCE' AND ap.UID=a.UID) AS 'EXPERIENCE',
+			(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='ELIGIBILITY' AND ap.UID=a.UID) AS 'ELIGIBILITY',
+			(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='TRAINING' AND ap.UID=a.UID) AS 'TRAINING',
+			(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.GRADED_BY='$c' AND ap.CRITERIA_CODE='INTERVIEW' AND ap.UID=a.UID) AS 'INTERVIEW',
+			(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='DEMO' AND ap.UID=a.UID) AS 'DEMO',
+			(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='COMMUNICATION' AND ap.UID=a.UID) AS 'COMMUNICATION',
+			((SELECT SUM(EQUIVALENT_POINTS) from applicants_points ap where (ap.CRITERIA_CODE = 'INTERVIEW' AND ap.UID=a.UID)) / (SELECT COUNT(ap.NO) from applicants_points ap where (ap.CRITERIA_CODE = 'INTERVIEW' AND ap.UID=a.UID))) AS `INTERVIEW_AVG`,
+			((SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='EDUCATION' AND ap.UID=a.UID)+(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='EXPERIENCE' AND ap.UID=a.UID)+(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='ELIGIBILITY' AND ap.UID=a.UID)+(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='TRAINING' AND ap.UID=a.UID)+(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='DEMO' AND ap.UID=a.UID)+(SELECT ap.EQUIVALENT_POINTS FROM applicants_points ap WHERE ap.CRITERIA_CODE='COMMUNICATION' AND ap.UID=a.UID)+((SELECT SUM(EQUIVALENT_POINTS) from applicants_points ap where (ap.CRITERIA_CODE = 'INTERVIEW' AND ap.UID=a.UID)) / (SELECT COUNT(ap.NO) from applicants_points ap where (ap.CRITERIA_CODE = 'INTERVIEW' AND ap.UID=a.UID)))) AS 'TOTALPOINTS'
+			FROM application a JOIN personal_info p JOIN user u ON p.UID=a.UID AND u.UID=p.UID WHERE a.PID='$a' AND (p.LASTNAME LIKE '%$b%' OR p.FIRSTNAME LIKE '%$b%' OR p.MIDDLENAME LIKE '%$b%' OR p.UID LIKE '%$b%' OR u.EMAIL LIKE '%$b%') ORDER BY TOTALPOINTS DESC;";
+
+		$result = mysqli_query($conn,$sql);
+		while ($row = mysqli_fetch_assoc($result)) {
+
+			$cnt += 1;
+			$id = $row['UID'];
+			$name = $row['LASTNAME'].', '.$row['FIRSTNAME'].' '.$row['MIDDLENAME'];
+			$address = $row['RESIDENTIAL_ADDRESS'];
+			$mobile = $row['MOBILE_NO'];
+			$email = $row['EMAIL'];
+			$educ = $row['EDUCATION'];
+			$gwa = $row['EDUCATION_GWA'];
+			$points = split(',', $gwa);
+			$exp = $row['EXPERIENCE'];
+			$eligib = $row['ELIGIBILITY'];
+			$train = $row['TRAINING'];
+			$interview = $row['INTERVIEW'];
+			$demo = $row['DEMO'];
+			$comm = $row['COMMUNICATION'];
+			$total_score = $row['TOTALPOINTS'];
+
+			if ($total_score==0) {
+				$rem = "-";
+			}
+			else{
+				$rem = number_format($total_score, 2, '.','');
+			}
+
+			if ($points[1]==2) {
+				$checkbox_html = "
+					<div class='ewc-grid-input'>
+						<input type='checkbox' class='checkbox-master' name='master'/>&nbsp;&nbsp;With MA or MS degree
+					</div>
+					<div class='ewc-grid-input'>
+						<input type='checkbox' class='checkbox-doctor' name='doctor' checked='true'/>&nbsp;&nbsp;With Master or Doctors(PhD) degree
+					</div>";
+			}
+			else{
+				$checkbox_html = "
+					<div class='ewc-grid-input'>
+						<input type='checkbox' class='checkbox-master' name='master' checked='true'/>&nbsp;&nbsp;With MA or MS degree
+					</div>
+					<div class='ewc-grid-input'>
+						<input type='checkbox' class='checkbox-doctor' name='doctor'/>&nbsp;&nbsp;With Master or Doctors(PhD) degree
+					</div>";
+			}
+			
+			$html .= "<section class= 'info-container'>
+							<section class= 'content'>
+								<p>$cnt</p>
+								<p class='applicant-id'>$id</p>
+							</section>
+							<section class= 'content'>
+								<p>$name</p>
+							</section>
+							<section class= 'content'>
+								<p>$address</p>
+							</section>
+							<section class= 'content'>
+								<p>$email</p>
+							</section>
+							<section class= 'content center-pos'>
+								<div class='eval-out'>
+									<button class='eval-out-editbtn'>Edit</button>
+									<div class='eval-out-value'>$educ</div>
+								</div>
+								<div class='educ-window'>
+									<div class='educ-window-content'>
+										<div class='ewc-grid'>
+											$checkbox_html
+										</div>
+										<div class='ewc-grid2'>
+											<input type='number' class='input-gwa' placeholder='Enter GWA' value='$points[0]'>
+											<div class='ewc-grid2-col'>
+												<p class='applicant-id'>$id</p>
+												<button class='ee-cancel'>Cancel</button>
+												<button class='ee-save'>Save</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</section>
+							<section class= 'content'>
+								<input type= 'text' name='EXPERIENCE' class='input-grade e-experience' value='$exp'>
+							</section>
+							<section class= 'content'>
+								<input type= 'text' name='ELIGIBILITY' class='input-grade e-let' value='$eligib'>
+							</section>
+							<section class= 'content'>
+								<input type= 'text' name='TRAINING' class='input-grade e-training' value='$train'>
+							</section>
+							<section class= 'content'>
+								<input type= 'text' name='INTERVIEW' class='input-grade e-interview' value='$interview'>
+							</section>
+							<section class= 'content'>
+								<input type= 'text' name='DEMO' class='input-grade e-demo' value='$demo'>
+							</section>
+							<section class= 'content'>
+								<input type= 'text' name='COMMUNICATION' class='input-grade e-communication' value='$comm'>
+							</section>
+							<section class= 'content'>
+								<p>".$rem."</p>
+							</section>
+						</section>";
+
+		}
+
+		// print_r($points);
+		print_r($html);
+
+	break;
+
 
 }
 
